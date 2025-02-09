9e645461c9ce2d1f0ba59a02c45311beb50c9b8b
Sayed Abdul <sakarim9124@gmail.com>
2025-02-09 15:10:56 +0530

    updated

--- python-project/src/crew_ai_project.py
+++ python-project/src/crew_ai_project.py
@@ -5,3 +5,15 @@ os.environ['OPEN_AI_KEY'] = ''
 from crewai import Agent
 from crewai_tools import SerperDevTool
 search_tool = SerperDevTool()
+
+# create a senior research agent with memory and verbose mode
+researcher = Agent(
+    role='senior_researcher',
+    goal='Uncover ground breaking technologies in { topic }',
+    verbose=True,
+    backstory=(
+        "Driven by curiosity, you are at the forefront of"
+        "innovation, eager to explore and share knowledge that could change "
+        "the world." 
+    )
+)

cb6a7661b68dc7f4fc26b7077eea30983681aff2
Sayed Abdul <sakarim9124@gmail.com>
2025-02-09 20:21:56 +0530

    updated

--- python-project/src/crew_ai_project.py
+++ python-project/src/crew_ai_project.py
@@ -1,9 +1,9 @@
 import os 
 from langchain_ollama import OllamaLLM
 
-os.environ['37fb1573261727028f8baab406e729707337c314'] = ''
+os.environ['SERPER_API_KEY'] = '37fb1573261727028f8baab406e729707337c314'
 
-from crewai import Agent
+from crewai import Agent, Task
 from crewai_tools import SerperDevTool
 search_tool = SerperDevTool()
 
@@ -13,6 +13,10 @@ llm = OllamaLLM(
     model="llama3.2:1b",
     verbose=True
 )
+
+topic = "quantum computing"
+
+### CREATE AGENTS START ###
 
 # create a senior research agent with memory and verbose mode
 researcher = Agent(
@@ -38,5 +42,39 @@ writer = Agent(
         "technological advancements to a broad audience."
     ),
     tools=[search_tool],
-    allow_delegation=True
+    allow_delegation=False
 )
+
+### CREATE AGENTS END ###
+
+
+### CREATE TASKS START ###
+
+# create a research task for the researcher agent
+research_task = Task(
+    description=(
+        "Identify the next big thing in {topic}."
+        "Focus on pros and cons and the overall narrative."
+        "Your final report should clearly articulate the key points"
+        "its market opportunity and potential risk."
+    ),
+    expected_output="A 2 paragraph report on the latest trends in {topic}.",
+    agent=researcher,
+    tools=[search_tool]
+)
+
+# create a writing task for the writer agent
+writing_task = Task(
+    description=(
+        "Write a report on the latest advancements in {topic}."
+        "Your report should be engaging, informative, and accessible to a "
+        "broad audience. Focus on the key benefits and potential impact of "
+        "the technology."
+    ),
+    expected_output="A 100-word article on {topic}.",
+    agent=writer,
+    async_execution=False,
+    tools=[search_tool]
+)
+
+### CREATE TASKS END ###
